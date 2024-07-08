a863fcd162fb69f4c419f4644c132adc45bb871f
Vero_bel <veronika_sia@mail.ru>
2024-07-08 17:50:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from datetime import time
+
+
+if __name__ == 'main':
+   calculate_salary()
+   get_employees() 
+   time()
